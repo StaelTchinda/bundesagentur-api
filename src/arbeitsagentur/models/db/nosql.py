6dbbd843e05e4c46d3ea31900f0c4b626e3e6351
Jonathan Ross <wonnyjony@gmail.com>
2024-05-01 16:50:55 +0200

    Added super simple DetailedApplicantsDb class for saving applicant details

--- src/arbeitsagentur/models/db/nosql.py
+++ src/arbeitsagentur/models/db/nosql.py
@@ -7,30 +7,42 @@ from tinydb.queries import QueryLike
 from tinydb.table import Document
 
 from src.arbeitsagentur.models.enums import *
-from src.arbeitsagentur.models.response import Bewerber
+from src.arbeitsagentur.models.response import BewerberUebersicht
+from src.arbeitsagentur.models.response import BewerberDetail
 
 
 PathLike = Union[Path, Text]
 
-
-class SearchedApplicantsDb:
-    def __init__(self, db_path: PathLike = "data/db/applicants.json"):
+class DetailedApplicantsDb:
+    def __init__(self, db_path: PathLike = "data/db/applicants_detail.json"):
         self.db = TinyDB(db_path)
 
-    def insert(self, applicant: Bewerber):
+    def insert(self, applicant: BewerberDetail):
         query = Query()
         if self.db.contains(query.refnr == applicant.refnr):
             raise ValueError(f"Document with refnr {applicant.refnr} already exists.")
         applicant_serializable_dict = self._serialize_object_(applicant)
         self.db.insert(applicant_serializable_dict)
 
-    def get_by_refnr(self, refnr: Text) -> Optional[Bewerber]:
+
+class SearchedApplicantsDb:
+    def __init__(self, db_path: PathLike = "data/db/applicants.json"):
+        self.db = TinyDB(db_path)
+
+    def insert(self, applicant: BewerberUebersicht):
+        query = Query()
+        if self.db.contains(query.refnr == applicant.refnr):
+            raise ValueError(f"Document with refnr {applicant.refnr} already exists.")
+        applicant_serializable_dict = self._serialize_object_(applicant)
+        self.db.insert(applicant_serializable_dict)
+
+    def get_by_refnr(self, refnr: Text) -> Optional[BewerberUebersicht]:
         doc: Optional[Document | List[Document]] = self.db.get(Query().refnr == refnr)
         if doc is None:
             return None
         elif isinstance(doc, list):
-            return Bewerber(**doc[0])
-        return Bewerber(**doc)
+            return BewerberUebersicht(**doc[0])
+        return BewerberUebersicht(**doc)
 
     def get(self, query: QueryLike):
         return self.db.search(query)
@@ -41,7 +53,7 @@ class SearchedApplicantsDb:
     def update(self, query: QueryLike, data):
         self.db.update(data, query)
 
-    def upsert(self, applicant: Bewerber):
+    def upsert(self, applicant: BewerberUebersicht):
         query = Query()
         if self.db.contains(query.refnr == applicant.refnr):
             serializable_dict = self._serialize_object_(applicant)
@@ -61,7 +73,7 @@ class SearchedApplicantsDb:
     def __del__(self):
         self.db.close()
 
-    def _serialize_object_(self, applicant: Bewerber) -> Dict:
+    def _serialize_object_(self, applicant: BewerberUebersicht) -> Dict:
         applicant_json = json.dumps(applicant.__dict__, default=str)
         applicant_serializable_dict = json.loads(applicant_json)
         return applicant_serializable_dict
