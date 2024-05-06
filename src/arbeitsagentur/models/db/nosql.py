559505ce9c6301fc67927c4b32bf1307fb04b912
stal.tchinda <stal.tchinda@check24.de>
2024-05-06 19:48:41 +0200

    Reset file to dev content

--- src/arbeitsagentur/models/db/nosql.py
+++ src/arbeitsagentur/models/db/nosql.py
@@ -25,13 +25,32 @@ class DetailedApplicantsDb:
         applicant_serializable_dict = self._serialize_object_(applicant)
         self.db.insert(applicant_serializable_dict)
 
+    def get(self, query: QueryLike):
+        return self.db.search(query)
+    
+    def get_by_refnr(self, refnr: Text) -> Optional[BewerberDetail]:
+        doc: Optional[Document | List[Document]] = self.db.get(Query().refnr == refnr)
+        if doc is None:
+            return None
+        elif isinstance(doc, list):
+            return BewerberDetail(**doc[0])
+        return BewerberDetail(**doc)
+
     def _serialize_object_(self, applicant: BewerberDetail) -> Dict:
-        applicant_json = json.dumps(applicant.__dict__, default=default_json_dumps)
+        applicant_json = json.dumps(applicant.__dict__, default=str)
         applicant_serializable_dict = json.loads(applicant_json)
         return applicant_serializable_dict
     
     def _unserealize_object_(self, applicant_dict: Dict) -> BewerberDetail:
         return BewerberDetail(**applicant_dict)
+    
+    def upsert(self, applicant: BewerberDetail):
+        query = Query()
+        if self.db.contains(query.refnr == applicant.refnr):
+            serializable_dict = self._serialize_object_(applicant)
+            self.db.upsert(serializable_dict, Query().refnr == applicant.refnr)
+        else:
+            self.insert(applicant)
 
 
 class SearchedApplicantsDb:
@@ -84,6 +103,14 @@ class SearchedApplicantsDb:
         self.db.close()
 
     def _serialize_object_(self, applicant: BewerberUebersicht) -> Dict:
+        def default_json_dumps(obj: Any):
+            if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
+                return str(obj)
+            elif isinstance(obj, Enum):
+                return obj.value
+            elif hasattr(obj, "__dict__"):
+                return obj.__dict__
+            return str(obj)
         applicant_json = json.dumps(applicant.__dict__, default=default_json_dumps)
         applicant_serializable_dict = json.loads(applicant_json)
         return applicant_serializable_dict
@@ -104,13 +131,3 @@ class LocalSearchParameters:
     disability: Union[Disability, None]
     page: Union[int, None]
     size: Union[int, None]
-
-
-def default_json_dumps(obj: Any):
-    if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
-        return str(obj)
-    elif isinstance(obj, Enum):
-        return obj.value
-    elif hasattr(obj, "__dict__"):
-        return obj.__dict__
-    return str(obj)
