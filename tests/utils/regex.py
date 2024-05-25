28f6e3339fc587f35d7ef77c712d73000cebf5cd
stal.tchinda <stal.tchinda@check24.de>
2024-05-25 22:15:46 +0200

    Create tests for healthcheck, arbeitsagentur paths and some paths for extended Add packages to requirements.txt Update README.md

---
+++ tests/utils/regex.py
@@ -0,0 +1,30 @@
+import re
+from typing import Any, Dict, List, Text, Union
+
+
+def search_regex_in_deep(
+    regex: Text, 
+    obj: Union[Text, Dict[Text, Any], List[Text], List[Dict[Text, Any]]], 
+    flags: Union[int, re.RegexFlag] = 0) -> bool:
+    did_match: bool = False
+
+    if isinstance(obj, dict):
+        for value in obj.values():
+            did_match = did_match or search_regex_in_deep(regex, value, flags)
+            if did_match:
+                return did_match
+    elif isinstance(obj, list):
+        for item in obj:
+            if isinstance(item, dict) \
+                or isinstance(item, str) \
+                or (isinstance(item, list) and len(item) > 0 and isinstance(item[0], dict)) \
+                or (isinstance(item, list) and len(item) > 0 and isinstance(item[0], str)):
+                did_match = did_match or search_regex_in_deep(regex, item, flags)
+                if did_match:
+                    return did_match
+    elif isinstance(obj, str):
+        did_match = did_match or bool(re.search(regex, obj, flags))
+        if did_match:
+            return did_match
+
+    return did_match
