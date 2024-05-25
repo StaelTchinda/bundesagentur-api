87a441b05a7741da9ad162b488ec1cb514933e57
stal.tchinda <stal.tchinda@check24.de>
2024-05-25 14:02:43 +0200

    Inject path parameters to route with dependency

--- src/applicants/schemas/arbeitsagentur/request.py
+++ src/applicants/schemas/arbeitsagentur/request.py
@@ -1,20 +1,21 @@
-from typing import Union, Text, Dict
+from typing import Optional, Text, Dict
 from pydantic import BaseModel
 from src.applicants.schemas.arbeitsagentur.enums import EducationType, LocationRadius, OfferType, WorkingTime, WorkExperience, ContractType, Disability
 
 
 class SearchParameters(BaseModel):
-    searchKeyword: Union[Text, None]
-    educationType: Union[EducationType, None]
-    locationKeyword: Union[Text, None]
-    locationRadius: Union[LocationRadius, None]
-    offerType: Union[OfferType, None]
-    workingTime: Union[WorkingTime, None]
-    workExperience: Union[WorkExperience, None]
-    contractType: Union[ContractType, None]
-    disability: Union[Disability, None]
-    page: Union[int, None]
-    size: Union[int, None]
+    searchKeyword: Optional[Text] = None
+    educationType: EducationType = EducationType.UNDEFINED
+    locationKeyword: Optional[Text] = None
+    locationRadius: LocationRadius = LocationRadius.ZERO
+    offerType: OfferType = OfferType.WORKER
+    workingTime: WorkingTime = WorkingTime.UNDEFINED
+    workExperience: WorkExperience = WorkExperience.WITH_EXPERIENCE
+    contractType: ContractType = ContractType.UNDEFINED
+    disability: Disability = Disability.UNDEFINED
+    page: int = 1
+    size: int = 25
+
 
 SEARCH_PARAMETERS_TO_GET_PARAMS: Dict[Text, Text] = {
     "searchKeyword": "was",
