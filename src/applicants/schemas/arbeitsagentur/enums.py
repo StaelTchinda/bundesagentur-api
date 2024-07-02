810fb4c52c73884af86aaae692b4b2e2bca12acf
StaelTchinda <staelwilfried@gmail.com>
2024-07-02 18:33:55 +0200

    refactor: Fix enum instantiation issue in ParamEnum class

--- src/applicants/schemas/arbeitsagentur/enums.py
+++ src/applicants/schemas/arbeitsagentur/enums.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Any, Callable
 
 
 class ParamEnum(Enum):
@@ -8,6 +9,8 @@ class ParamEnum(Enum):
 
     @classmethod
     def __flex_init__(cls, value_or_param_value, param_value = None) -> 'ParamEnum':
+        compare_nullable: Callable[[Any, Any], bool] = lambda a, b: a == b or (a is None and b is None)
+
         _value_ = None
         _param_value_ = None
         if param_value is not None:
@@ -15,14 +18,16 @@ class ParamEnum(Enum):
             _param_value_ = param_value
         else:
             for enum_entity in cls:
-                if enum_entity.value == value_or_param_value \
-                        or enum_entity.param_value == value_or_param_value:
+                if compare_nullable(enum_entity.value, value_or_param_value) \
+                        or compare_nullable(enum_entity.param_value, value_or_param_value):
                     _value_ = enum_entity.value
                     _param_value_ = enum_entity.param_value
-        if _value_ is None or _param_value_ is None:
-            raise ValueError(f"Invalid parameters for class {cls.__name__}: {value_or_param_value}, {param_value}")
         
-        enum_value = cls(_value_, _param_value_)
+        try:
+            enum_value = cls(_value_, _param_value_)
+        except Exception as e:
+            raise ValueError(f"Could not instantiate enum {cls.__name__} with parameters ({value_or_param_value}, {param_value}): {e}")
+
         return enum_value
 
     @classmethod
